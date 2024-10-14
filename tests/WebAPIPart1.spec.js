9a652588e5057f4ab06ab06559e6efa02f79f148
sharifzodah <sharifzodah@gmail.com>
2024-10-14 10:29:47 -0400

    Delete functionality test fixed by adding explicit wait of 500ms

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -21,39 +21,43 @@ test.beforeAll( async ()=>
 
 
 // @positive-testCase
-test.skip('Place Order test', async ({page})=>
-    {
-        await page.addInitScript(value => {
-            window.localStorage.setItem('token', value);
-        }, token);
+    test('Place Order test', async ({page})=>
+        {
+            await page.addInitScript(value => {
+                window.localStorage.setItem('token', value);
+            }, token);
 
-        const ccNumber = '4587 8874 6621 3315';
-        const cvv = '988';
-        const name = 'QA SDET';
-        const coupon = 'rahulshettyacademy';
-        const ccDetails = [ccNumber, cvv, name, coupon];
-        const countryName = 'United States'
-                
-        await page.goto("https://rahulshettyacademy.com/client/");
+            const ccNumber = '4587 8874 6621 3315';
+            const cvv = '988';
+            const name = 'QA SDET';
+            const coupon = 'rahulshettyacademy';
+            const ccDetails = [ccNumber, cvv, name, coupon];
+            const countryName = 'United States'
+                    
+            await page.goto("https://rahulshettyacademy.com/client/");
 
-        await page.locator('.card-body b').first().waitFor();
-        await page.locator('.card-body').filter({hasText: "IPHONE 13 PRO"})
-        .getByRole("button", {name: "Add To Cart"}).click();
-        await page.getByRole("listitem").getByRole("button", {name: "Cart"}).click();
-        await page.locator("div li").first().waitFor();
-        await expect(page.getByText("IPHONE 13 PRO")).toBeVisible();
-        await page.getByRole("button", {name: "Checkout"}).click();
-        await page.getByPlaceholder("Select Country").pressSequentially("united");
-        await page.getByRole("button", {name: "United States"}).nth(1).click();
-        await page.getByText("PLACE ORDER").click();
-        await expect(page.getByText("Thankyou for the order.")).toBeVisible();
-        const orderId = await page.locator("td label").nth(1).textContent();
+            await page.locator('.card-body b').first().waitFor();
+            await page.locator('.card-body').filter({hasText: "IPHONE 13 PRO"})
+            .getByRole("button", {name: "Add To Cart"}).click();
+            await page.getByRole("listitem").getByRole("button", {name: "Cart"}).click();
+            await page.locator("div li").first().waitFor();
+            await expect(page.getByText("IPHONE 13 PRO")).toBeVisible();
+            await page.getByRole("button", {name: "Checkout"}).click();
+            await page.getByPlaceholder("Select Country").pressSequentially("united");
+            await page.getByRole("button", {name: "United States"}).nth(1).click();
+            await page.getByText("PLACE ORDER").click();
+            await expect(page.getByText("Thankyou for the order.")).toBeVisible();
+            const orderId = await page.locator("td label").nth(1).textContent();
 
-    });
+        });
 
      // @positive-testCase
      test('Delete 1 order w/Special Locators test', async ({page})=>
         {
+            await page.addInitScript(value => {
+                window.localStorage.setItem('token', value);
+            }, token);
+
             const ccNumber = '4587 8874 6621 3315';
             const cvv = '988';
             const name = 'QA SDET';
@@ -77,8 +81,9 @@ test.skip('Place Order test', async ({page})=>
             await page.locator('[role = "alert"]').waitFor();
             console.log(await page.locator('[role = "alert"]').textContent());
             await page.waitForTimeout(500);
+            await page.waitForLoadState("networkidle");
             const remainingOrderCount = await page.locator("tbody tr").count();
             console.log('Remaining Order Count: ', remainingOrderCount);
-            expect(remainingOrderCount).toBe((initialOrderCount - 2));            
+            expect(remainingOrderCount).toBe((initialOrderCount - 1));            
 
         });
