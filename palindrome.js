70abc6fb7bde95db4758970832b535e0ed5b6f1f
sharifzodah <sharifzodah@gmail.com>
2024-11-05 17:40:47 -0500

    adding palindrome function; js learning

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function isStringPalindrome(str) {
+    if(str === null){
+        return null;
+    }
+    if (str.length === 0) {
+        return "No text provided";
+    }
+    if (str.length === 1) {
+        return str;
+    }
+
+    for (let i = 0, j = str.length-1; i < j; i++, j--) {
+        return str[i] !== str[j] ? false : true;
+    }
+}
+s = null;
+const result = isStringPalindrome(s);
+console.log(result)
